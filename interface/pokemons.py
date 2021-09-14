00b667b8aead3059f8ac49c8129b522124b4b1d8
Ramiro Detke <ramiro.fer.det@gmail.com>
2021-09-14 18:30:41 -0300

    fix pokemons if

--- interface/pokemons.py
+++ interface/pokemons.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 import jsonpickle
 
-
 endpoint = Blueprint('pokemon', __name__)
 
 
@@ -11,14 +10,16 @@ def all_pokemons():
     instances = []
     max = None
     try:
-        from juego.pokemon import get_all_pokemons
         from juego.personaje import MAX_POKEMONS
-        instances = get_all_pokemons()
         max = MAX_POKEMONS
+
+        from juego.pokemon import get_all_pokemons
+        instances = get_all_pokemons()
+
 
     finally:
         response = {
-            'all_pokemons': instances, 
+            'all_pokemons': instances,
             'max_pokemons': max
         }
         return jsonpickle.encode(response)
